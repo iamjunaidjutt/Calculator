616c923b11ef027bdfdf9a560177dc6d673894b2
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2022-07-16 16:21:32 +0500

    Calculator Completed

--- calculator.js
+++ calculator.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res) {
-    res.send("Thanks for posting!");
+    let num1 = +req.body.num1;
+    let num2 = +req.body.num2; 
+    let result = num1 + num2;
+    res.send("Sum is " + result);
 });
 
 app.listen(3000, function() {
